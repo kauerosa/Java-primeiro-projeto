375a23d03c7ff30de553db1f78018d4e3316d7d8
Kauêrosa <kauerosa2002@gmail.com>
2023-09-02 22:40:30 -0300

    Mues Primeiro Projetos em Java

---
+++ src/BoletimEsrudantil.java
@@ -0,0 +1,13 @@
+
+public class BoletimEsrudantil {
+
+    public static void main (String [] args) {
+        int mediaFinal = 8;
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if(mediaFinal == 6)
+            System.out.println("Prova Final");
+        else
+            System.out.println("Aprovado");
+    }    
+}
---
+++ src/MinhaClase.java
@@ -0,0 +1,7 @@
+public class MinhaClase {
+public static void main (String[] args) {
+        
+        System.out.print("Olá, meu nome é Kah.");
+
+    }
+}
